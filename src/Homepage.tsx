1ad1b4dfca1033cd97e19b0de35e19cc1c869d60
modtherhub <moibhub@gmail.com>
2025-07-01 21:58:55 +0400

    add backgorunds

---
+++ src/Homepage.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    
+<div className="grid grid-cols-5 grid-rows-5 gap-4">
+        <div className="col-start-2">2</div>
+    <div className="col-start-3 row-start-2">3</div>
+    <div className="col-start-4 row-start-1">4</div>
+    <div className="col-start-4 row-start-3">5</div>
+    <div className="col-start-2 row-start-3">6</div>
+</div>
+    
+  )
+}
+
+export default Homepage
