5a01495b98b6efec0fb939825892a38c18ab36f1
modtherhub <moibhub@gmail.com>
2025-07-01 22:43:52 +0400

    add backgorunds

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import { AuroraBackground } from "./components/ui/aurora-background"
+import Navbar from "./components/Navbar"
 
 function App() {
   
   return (
     <>
-    <AuroraBackground>
+    <Navbar/>
+    <section className="min-h-screen w-full relative 
+    bg-gradient-to-r from-fuchsia-500  via-red-400 to-orange-600
+    flex items-center justify-center text-white">
+      <AuroraBackground >
       <div>
         
       </div>
-    </AuroraBackground>
+      </AuroraBackground>
+    </section>
+    
     </>
+    
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
+    const Listitem: string[] = [
+        'Work',
+        'Services'
+    ]
+    return (
+        <header className='fixed bg-neutral-400/20 backdrop-blur-[3px] 
+        flex justify-between items-center gap-14 py-3 px-10
+        z-10 border border-neutral-400/20 rounded-full
+        left-1/2 translate-x-[-50%] top-[10px]
+        shadow-lg text-white'>
+
+            <ul className='flex gap-6 text-xl'>
+
+            </ul>
+
+            <button className='bg-gradient-to-r
+                from-blue-500 to-green-400 px-4 py-1
+                rounded-full shadow-2xl hover:from-blue-600
+                hover:to-green-600 transition-all duration-500'>
+                Contact
+            </button>
+
+        </header>
   )
 }
 
