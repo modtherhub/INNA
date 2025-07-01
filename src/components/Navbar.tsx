a69e74cd168fde45e449bc6c83b74ac12d85c4d3
modtherhub <moibhub@gmail.com>
2025-07-02 03:29:43 +0400

    Navbar is done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
 
 const Navbar = () => {
     const Listitem: string[] = [
+        'Home',
+        'Services',
         'Work',
-        'Services'
+        'Blog',
+        'Product'
     ]
     return (
         <header className='fixed bg-neutral-400/20 backdrop-blur-[3px] 
@@ -13,10 +15,20 @@ const Navbar = () => {
         shadow-lg text-white'>
 
             <ul className='flex gap-6 text-xl'>
+                {Listitem.map((item, index) => (
 
+                    <li className='text-sm relative group cursor-pointer' key={index}>
+                        {item}
+                        <span className="absolute left-0 bottom-[-5px] 
+                        w-0 h-[2px] group-hover:w-full bg-gradient-to-r
+                        from-blue-500 to-green-400 rounded-full
+                        transition-all duration-300"></span>
+                    </li>
+                    
+                ))}
             </ul>
 
-            <button className='bg-gradient-to-r
+            <button className='bg-gradient-to-r text-sm
                 from-blue-500 to-green-400 px-4 py-1
                 rounded-full shadow-2xl hover:from-blue-600
                 hover:to-green-600 transition-all duration-500'>
